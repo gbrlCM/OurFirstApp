1fa9b115514533477b9a771c1d98ddfd4acbcd53
Marcos Chevis Meira <marcoschevism@gmail.com>
2021-06-03 14:01:23 -0300

    game result table view

---
+++ NanoChallenge-MyFirstApp/Game/Models/Card.swift
@@ -0,0 +1,19 @@
+//
+//  Card.swift
+//  NanoChallenge-MyFirstApp
+//
+//  Created by Anna Carolina Costa Andrade on 02/06/21.
+//
+
+import Foundation
+import UIKit
+
+struct Card {
+    var subject: String
+    var isCorrect: Bool
+    var cardImage: UIImage?
+    var subjectExplanation: String
+    var answerExplanation: String
+    var userGetItRight: Bool
+
+}
