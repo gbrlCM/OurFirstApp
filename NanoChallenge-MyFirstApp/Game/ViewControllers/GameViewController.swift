96bebae3a2f0d634e9408672b0c15bf66ca9862d
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-06-18 13:32:56 -0300

    Final touches

--- NanoChallenge-MyFirstApp/Game/ViewControllers/GameViewController.swift
+++ NanoChallenge-MyFirstApp/Game/ViewControllers/GameViewController.swift
@@ -141,15 +141,6 @@ class GameViewController: UIViewController {
         destination.titleString = gameService.actualCard?.subject.rawValue ?? ""
         destination.bodyString = gameService.actualCard?.subjectExplanation ?? ""
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
